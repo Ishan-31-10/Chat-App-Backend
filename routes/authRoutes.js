0545fbc4e1c739d642c21f1c6df23502edf964af
Ishan-31-10 <ishan31fz@gmail.com>
2025-09-02 02:15:44 +0530

    Change database

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
+import User from "../models/User.js"; 
 
 const router = express.Router();
 
